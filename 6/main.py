36681fdd3470533e27251dc3a3d0ca7d95d27a65
Teijo Pekkola <teijo.pekkola@hotmail.com>
2023-12-06 11:22:24 +0200

    Tiedoston avaamisen elegantisointi

--- 6/main.py
+++ 6/main.py
@@ -2,10 +2,8 @@
 from numpy import prod
 
 # %% 1
-with open('input.txt') as file:
-    print(prod([sum((race[0]-t)*t > race[1] for t in range(race[0])) for race in zip(*[[int(cell) for cell in line.strip().split(' ')[1:] if cell] for line in file.readlines()])]))
+print(prod([sum((race[0]-t)*t > race[1] for t in range(race[0])) for race in zip(*[[int(cell) for cell in line.strip().split(' ')[1:] if cell] for line in open("input.txt", 'r')])]))
 
 # %% 2
-with open('input.txt') as file:
-    print(prod([sum((race[0]-t)*t > race[1] for t in range(race[0])) for race in zip(*[[int(''.join(line.strip().split(' ')[1:]))] for line in file.readlines()])]))
+print(prod([sum((race[0]-t)*t > race[1] for t in range(race[0])) for race in zip(*[[int(''.join(line.strip().split(' ')[1:]))] for line in open("input.txt", 'r')])]))
 

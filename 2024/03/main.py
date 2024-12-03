fbb3ef5f6c92f908eb174dd5beedaaa9091078fa
Teijo <teijo.pekkola@example.fi>
2024-12-03 09:45:20 +0200

    Day 3 speedrun 100%

---
+++ 2024/03/main.py
@@ -0,0 +1,48 @@
+#%%
+
+from re import finditer
+from operator import mul
+
+# FILENAME = 'example_input.txt
+FILENAME = 'input.txt'
+
+
+def load_data(filename: str, header: int = None):
+    with open(filename, 'r') as file:
+        return file.read()
+data = load_data(FILENAME)
+
+
+def mult_finder(data):
+    for match in finditer(r'mul\((\d\d?\d?),(\d\d?\d?)\)', data):
+        yield int(match[1]), int(match[2])
+
+# def do_finder(data):
+#     for match in finditer(r'do\(\)', data):
+#         yield match.start()
+
+# def dont_finder(data):
+#     for match in finditer(r'don\'t\(\)', data):
+#         yield match.start()
+    
+    
+def one_finder_to_rule_them_all(data):
+    yield_switch = True
+    for match in finditer(r'(?P<do>do\(\))|(?P<dont>don\'t\(\))|mul\((\d\d?\d?),(\d\d?\d?)\)', data):
+        if match.group('do'):
+            yield_switch = True
+        elif match.group('dont'):
+            yield_switch = False
+        elif yield_switch:
+            yield int(match[3]), int(match[4])
+    
+    
+    
+if __name__ == "__main__":
+    
+    task_1 = sum(map(mul, *zip(*mult_finder(data))))
+    print(f"{task_1 = }")
+        
+    task_2 = sum(map(mul, *zip(*one_finder_to_rule_them_all(data))))
+    print(f"{task_2 = }")
+

1a949b73a23fc7175af59087dcea2f4cd90c8111
Teijo <teijo.pekkola@example.fi>
2024-12-07 15:44:49 +0200

    Some progression was made
    Results are now consistent, but consistently wrong

--- 2024/06/main.py
+++ 2024/06/main.py
@@ -1,7 +1,6 @@
 #%% ----------- SETUP -------------------------
 import os
 from sys import path as SYSPATH
-from math import inf
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,27 +11,27 @@ from utils import load_data, execute_function
 FILENAME = 'example_input.txt'
 FILENAME = 'input.txt'
 
-
 class GuardMap:
     directions = ((0, -1), (1, 0), (0, 1), (-1, 0)) #URDL
-    rotate_history = set()
-    same_rotate_position_patience = 0
-    same_rotate_position_patience_max = 10 # TÄTÄ VOI VAIHTAA, MUTTA EI PITÄISI VAIKUTTAA TULOKSEEN, MUTTA KYLLÄ VAIKUTTAA PRKL!!
+    rotate_history: set[tuple[int, int, int]] = None # set definition must be in __init__ ('mutable default argument pitfall')
     looping = False
     def __init__(self, filename, custom_obstacle_position: tuple[int,int]|None = None):
         self.data = self._load_data(filename)
         self._direction_index = 0
-        self.position = self._where_am_i()
-        if custom_obstacle_position and self.is_same_position(self.position, custom_obstacle_position):
+        self.rotate_history = set()
+        self.position = self._find_starting_position()
+        
+        # Set obstacle somewhere, if defined
+        if custom_obstacle_position and not self.is_same_position(self.position, custom_obstacle_position):
             self.try_set(*custom_obstacle_position, 0)
         
     def _load_data(self, filename: str) -> tuple[tuple[int]]:
         file_path = os.path.join(script_path, filename)
         def str2int(s):
             if s == '#': return 0 # Obstacles are 0
-            elif s == '.': return -1
-            elif s == '^': return 1
-            else: raise Exception('Invalid char in map.')
+            elif s == '.': return -1 # Empty space is negative
+            elif s == '^': return 1 # Footprints are 1,2,3,4 (all directions)
+            else: raise Exception('Invalid character in map.')
         return load_data(
             file_path, 
             matrix=True,
@@ -42,15 +41,18 @@ class GuardMap:
     def is_same_position(self, pos_1: tuple[int,int], pos_2: tuple[int,int]) -> bool:
         return all(p1==p2 for p1,p2 in zip(pos_1, pos_2))
     
-    def rotate_direction(self):
-        if any(self.is_same_position(self.position, pos) for pos in self.rotate_history):
-            self.same_rotate_position_patience += 1
-        else:
-            self.same_rotate_position_patience = 0
+    def i_have_rotated_here_before(self):
+        my_state = (*self.position, self._direction_index)
+        return any(
+            all(a==b for a,b in zip(my_state, history_node))
+            for history_node in self.rotate_history
+        )
         
-        if self.same_rotate_position_patience > self.same_rotate_position_patience_max:
+    
+    def rotate_direction(self):
+        if self.i_have_rotated_here_before():
             self.looping = True
-        self.rotate_history.add(tuple(self.position))
+        self.rotate_history.add((*self.position, self._direction_index))
         self._direction_index = (self._direction_index + 1)%len(self.directions)
         self.previous_rotate = self.position
     
@@ -58,7 +60,7 @@ class GuardMap:
     def direction(self) -> tuple[int,int]:
         return self.directions[self._direction_index]
     
-    def _where_am_i(self):
+    def _find_starting_position(self):
         for y, row in enumerate(self.data):
             try:
                 return (row.index(1), y)
@@ -116,6 +118,9 @@ class GuardMap:
     @staticmethod
     def translate_position(position: tuple[int,int], direction: tuple[int,int]) -> tuple[int,int]:
         return tuple(map(sum, zip(position, direction)))
+    
+    def add_footprint(self) -> bool:
+        return self.try_set(*self.position, self._direction_index+1)
         
     def travel(self) -> bool:
         next_pos = None
@@ -130,13 +135,13 @@ class GuardMap:
             
         
         self.position = next_pos
-        return self.try_set(*next_pos, self._direction_index+1)
+        position_in_bounds = self.add_footprint()
+        return position_in_bounds
         
     
-    def traverse_map(self, max_steps: int = inf):
+    def traverse_map(self) -> bool:
         travelling = True
-        steps = 0
-        while travelling and steps < max_steps:
+        while travelling:
             travelling = self.travel()
             if self.looping:
                 return True
@@ -168,11 +173,12 @@ def task_2():
     gm.traverse_map()
     possible_obstacles = gm.find_footprints()
     n_loops = 0
-    for po in tqdm(possible_obstacles, desc="Traversing universes", ascii=' 123456789#'):
+    for po in tqdm(possible_obstacles, 
+                   desc="Traversing universes", 
+                   ascii=' 123456789#'):
         gm = GuardMap(FILENAME, po)
         if gm.is_same_position(po, gm.position):
             continue
-        # I am in a loop if it feels like I'm in a loop
         is_loop = gm.traverse_map()
         if is_loop:
             n_loops += 1
